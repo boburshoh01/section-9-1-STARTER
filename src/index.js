4b7a4aa5c9435be3a0cf0f58c6eb47348fd592cf
boburshoh01 <temirovbobur8@gmail.com>
2023-11-07 14:45:30 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { fromEvent, interval } from "rxjs";
+import { map, concatMap, take, tap } from "rxjs/operators";
+import { ajax } from "rxjs/ajax";
+
+const button = document.querySelector("#btn");
+const observable = fromEvent(button, "click").pipe(
+  concatMap(() => {
+    return ajax.getJSON("https://jsonplaceholder.typicode.com/todos/1").pipe(
+      take(5),
+      tap({
+        complete() {
+          console.log("inner observable completed");
+        },
+      })
+    );
+  })
+);
+
+const subscription = observable.subscribe({
+  next(value) {
+    console.log(value);
+  },
+  complete() {
+    console.log("complete");
+  },
+});
